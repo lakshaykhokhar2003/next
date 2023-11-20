3d76439d3305c1cc4493140c68957a47a4001a61
Lakshay <lakshaykhokhar2003@gmail.com>
2023-11-20 15:27:31 +0530

    ready for deployment

---
+++ components/meetups/MeetupDetails.js
@@ -0,0 +1,11 @@
+import styles from './MeetupDetails.module.css';
+
+const MeetupDetail = (props) => {
+    return (<section className={styles.detail}>
+        <img src={props.image} alt={props.title}/>
+        <h1>{props.title}</h1>
+        <address>{props.address}</address>
+        <p>{props.description}</p>
+    </section>);
+}
+export default MeetupDetail;
